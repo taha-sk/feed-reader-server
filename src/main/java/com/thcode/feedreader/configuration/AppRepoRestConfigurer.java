d56509d9e822bb39eab74d7f4122fe47873ac7c2
Taha Sukru Karabacakoglu <supersawer@gmail.com>
2021-05-14 21:49:32 +0300

    Exposing Widget Id for usage of ParamMap in Angular navigation

--- src/main/java/com/thcode/feedreader/configuration/AppRepoRestConfigurer.java
+++ src/main/java/com/thcode/feedreader/configuration/AppRepoRestConfigurer.java
@@ -6,6 +6,7 @@ import org.springframework.data.rest.core.event.ValidatingRepositoryEventListene
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import com.thcode.feedreader.model.Widget;
 import com.thcode.feedreader.model.WidgetType;
 import com.thcode.feedreader.validator.WidgetValidator;
 
@@ -14,6 +15,7 @@ public class AppRepoRestConfigurer implements RepositoryRestConfigurer {
 	
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Widget.class);
 		config.exposeIdsFor(WidgetType.class);
 	}
 	
